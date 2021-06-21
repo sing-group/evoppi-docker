847d54137e288759455c42b5d728235a2d8b4e74
Hugo López-Fernández <hlfernandez@uvigo.es>
2021-06-21 16:41:54 +0200

    Remove useless UPDATE gene query

--- db/evoppi-mysql-data.sql
+++ db/evoppi-mysql-data.sql
@@ -8,5 +8,3 @@ VALUES ('researcher', 'RESEARCHER', 'researcher@email.com', '%%RESEARCHER_PASSWO
 INSERT INTO `administrator` (`login`) VALUES ('admin');
 INSERT INTO `researcher` (`login`) VALUES ('researcher');
 
-UPDATE gene g
-SET g.defaultName = (SELECT name FROM gene_name_value WHERE geneId = g.id ORDER BY IF(name RLIKE '^[a-z]', 1, 2), name LIMIT 1);
